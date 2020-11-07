50bd680a49a93d73489b0a98ebf2372d91a4fe9b
gzim921 <gzimiseni7@gmail.com>
2020-11-07 16:49:33 +0100

    Add and remove instruction field on fly

--- app/javascript/components/createFields.js
+++ app/javascript/components/createFields.js
@@ -1,20 +1,41 @@
 $(document).ready(function() {
   const createInstruction = $('#addInstruction');
+  const removeInstruction = $('.fa-times');
 
-  createInstruction.click(function(){
-      var date = new Date();
-      var mSec = date.getTime();
+  createInstruction.click(function() {
+    var date = new Date();
+    var mSec = date.getTime();
 
-      idOrder = 
-         "recipe_instructions_0_order".replace("0", mSec);
-      nameOrder = 
-          "recipe[instructions][0][order]".replace("0", mSec);
+    idOrder = 
+      "recipe_instructions_attributes_0_order".replace("0", mSec);
+    nameOrder = 
+      "recipe[instructions_attributes][0][order]".replace("0", mSec);
 
-      idInstruction = 
-          "recipe_instructions_0_instruction_info".replace("0", mSec);
-      nameInstruction = 
-           "recipe[instructions][0][instruction_info]".replace("0", mSec);    
+    idInstruction = 
+      "recipe_instructions_attributes_0_instruction_info".replace("0", mSec);
+    nameInstruction = 
+      "recipe[instructions_attributes][0][instruction_info]".replace("0", mSec);    
 
-      $("ul").append("<li><div class='columns is-mobile'><div class='column is-3'><input class='input' " + nameOrder + " id=" + idOrder + " type='number' placeholder='Step'></div><div class='column'><input id=" + idInstruction + " name=" + nameInstruction + " class='input' type='text' placeholder='Instruction...'> </div></div></li>");
+    const htmlElement = `
+      <li>
+        <div class='columns is-mobile'>
+          <div class='column is-2'>
+            <input class='input' " + nameOrder + " id=" + idOrder + " type='number' placeholder='Step'>
+          </div>
+          <div class='column'>
+            <input id=" + idInstruction + " name=" + nameInstruction + " class='input' type='text' placeholder='Instruction...'>
+          </div>
+          <div class='column is-1'>
+            <div class='icon_times'>
+              <i class='fa fa-times fa-2x' ></i>
+            </div>
+          </div>
+        </div>
+      </li>`
+    $("ul").append(htmlElement);
+  });
+
+  $("ul").on('click', 'i',  function(){
+    $(this).parent().parent().parent().remove()
   });
 });
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,4 @@
 class Recipe < ApplicationRecord
   has_many :instructions
-  accepts_nested_attributes_for :instructions
+  accepts_nested_attributes_for :instructions, allow_destroy: true
 end
