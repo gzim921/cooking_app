1fa37e870902652120f49e0a767e2df7c476e980
gzim921 <gzimiseni7@gmail.com>
2020-11-07 17:36:05 +0100

    Form recipe refactored with partials

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe < ApplicationRecord
-  has_many :instructions
-  has_many :ingridients
+  has_many :instructions, dependent: :destroy
+  has_many :ingridients, dependent: :destroy
 
   accepts_nested_attributes_for :instructions, allow_destroy: true
   accepts_nested_attributes_for  :ingridients, allow_destroy: true
