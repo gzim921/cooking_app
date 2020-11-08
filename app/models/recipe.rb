3060a4380b5c53f6576209f6aefee82bee626b8c
gzim921 <gzimiseni7@gmail.com>
2020-11-08 17:19:55 +0100

    Modified nested forms

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,8 +5,8 @@ class Recipe < ApplicationRecord
   has_many :instructions, dependent: :destroy
   has_many :ingridients, dependent: :destroy
 
-  accepts_nested_attributes_for :instructions, allow_destroy: true
-  accepts_nested_attributes_for  :ingridients, allow_destroy: true
+  accepts_nested_attributes_for :instructions, allow_destroy: true, reject_if: :all_blank
+  accepts_nested_attributes_for  :ingridients, allow_destroy: true, reject_if: :all_blank
 
   validates :title, presence: true, length: { in: MIN_TITLE_LENGTH..MAX_TITLE_LENGTH }
   validates :description, presence: true, length: { maximum: MAX_DESC_LENGTH }
