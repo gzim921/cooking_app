8bfda5cf15ad884b62cd6ad70bd3d2e3a12b9fc1
gzim921 <gzimiseni7@gmail.com>
2020-11-08 18:15:01 +0100

    Refactoring recipe controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,10 +1,11 @@
 class RecipesController < ApplicationController
+  before_action :find_recipe, expect: [:index, :new, :create]
+
   def index
     @recipes = Recipe.all
   end
 
   def show
-    @recipe = Recipe.find(params[:id])
   end
 
   def new
@@ -23,11 +24,9 @@ class RecipesController < ApplicationController
   end
 
   def edit
-    @recipe = Recipe.find(params[:id])
   end
 
   def update
-    @recipe = Recipe.find(params[:id])
     if @recipe.update(recipe_params)
       redirect_to @recipe
     else
@@ -36,7 +35,6 @@ class RecipesController < ApplicationController
   end
 
   def destroy
-    @recipe = Recipe.find(params[:id])
     if @recipe.destroy
       redirect_to root_path
     else
@@ -48,5 +46,9 @@ class RecipesController < ApplicationController
 
   def recipe_params
     params.require(:recipe).permit(:title, :description, instructions_attributes: %i[id order instruction_info _destroy], ingridients_attributes: %i[id name _destroy])
+  end
+
+  def find_recipe
+    @recipe = Recipe.find(params[:id])
   end
 end
