1b91d7690e9bd70b2514eaf8afe88f516a541323
gzim921 <gzimiseni7@gmail.com>
2020-11-12 13:22:15 +0100

    Validations for ingridient model

--- app/models/ingridient.rb
+++ app/models/ingridient.rb
@@ -1,3 +1,16 @@
 class Ingridient < ApplicationRecord
+  MIN_NAME_LENGTH = 2
+  MAX_NAME_LENGTH = 40
+
+  before_create :capitalize_name
+
   belongs_to :recipe
+
+  validates :name, presence: true, length: { in: MIN_NAME_LENGTH..MAX_NAME_LENGTH }
+
+  private
+
+  def capitalize_name
+    self.name = name.capitalize
+  end
 end
