5b12c62a864160fe352c28b469ea5bcdc566cb7c
gzim921 <gzimiseni7@gmail.com>
2020-11-12 14:49:53 +0100

    User form for signing up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,20 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.create(user_params)
+
+    if @user.save
+        # redirect to User profile
+        redirect_to root_path
+    else
+        render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name,
+                                 :email, :password, :password_confirmation)
   end
 end
