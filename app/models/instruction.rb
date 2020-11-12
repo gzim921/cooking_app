4268d76b0f0ec6e2dca1241cfa73b794c0545e0b
gzim921 <gzimiseni7@gmail.com>
2020-11-12 13:12:51 +0100

    Validations for instruction model

--- app/models/instruction.rb
+++ app/models/instruction.rb
@@ -1,3 +1,10 @@
 class Instruction < ApplicationRecord
+  MIN_INS_LENGTH = 3
+  MAX_INS_LENGTH = 100
+
   belongs_to :recipe
+
+  validates :order, presence: true, numericality: { only_integer: true, greater_than: 0 }
+
+  validates :instruction_info, presence: true, length: { in: MIN_INS_LENGTH..MAX_INS_LENGTH }
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,11 +2,12 @@ class Recipe < ApplicationRecord
   MIN_TITLE_LENGTH = 3
   MAX_TITLE_LENGTH = 40
   MAX_DESC_LENGTH = 200
+
   has_many :instructions, dependent: :destroy
   has_many :ingridients, dependent: :destroy
 
   accepts_nested_attributes_for :instructions, allow_destroy: true, reject_if: :all_blank
-  accepts_nested_attributes_for  :ingridients, allow_destroy: true, reject_if: :all_blank
+  accepts_nested_attributes_for :ingridients, allow_destroy: true, reject_if: :all_blank
 
   validates :title, presence: true, length: { in: MIN_TITLE_LENGTH..MAX_TITLE_LENGTH }
   validates :description, presence: true, length: { maximum: MAX_DESC_LENGTH }
