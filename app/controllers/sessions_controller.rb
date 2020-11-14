ee46a4ee3744c72857e1f80bef9b24684050e183
gzim921 <gzimiseni7@gmail.com>
2020-11-14 15:00:31 +0100

    User login form and action created

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+               .try(:authenticate, params[:session][:password])
+
+    if user
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      render :new
+    end
   end
 end
