14c59d1df5beaa7d9049de378535818a23bcc28a
gzim921 <gzimiseni7@gmail.com>
2020-11-14 14:56:41 +0100

    Session controller and routes created

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  root to: 'recipes#index'
 
   resources :recipes do
-    resources :ingridients, :instructions, except: [:index, :show]
+    resources :ingridients, :instructions, except: [:index, :show]	    
   end
 
   get '/signup', to: 'users#new', as: :new_user
   resources :users, except: [:index, :new, :destroy]
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/login', to: 'sessions#destroy'
 end
