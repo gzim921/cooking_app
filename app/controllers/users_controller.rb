41601c41499f37a0763c7a7a1c2fac9c538fa14e
gzim921 <gzimiseni7@gmail.com>
2020-11-15 16:02:25 +0100

    User controller refactored

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+  before_action :require_login, only: [:eidt, :update]
+  before_action :find_user, except: [:new, :create]
+  before_action :correct_user, only: [:edit, :update]
+
+  def show
+  end
+
   def new
     @user = User.new
   end
@@ -15,40 +22,14 @@ class UsersController < ApplicationController
     end
   end
 
-  def show
-    @user = User.find(params[:id])
-  end
-
   def edit
-    if logged_in?
-      @user = User.find(params[:id])
-      unless user_equals?(@user)
-        flash[:danger] = 'You dont have permission!'
-        redirect_to root_path
-      end
-    else
-      flash[:danger] = 'You have to login!'
-      redirect_to login_path
-    end
   end
 
   def update
-    if logged_in?
-      @user = User.find(params[:id])
-
-      unless user_equals?(@user)
-        flash[:danger] = 'You dont have permission!'
-        redirect_to root_path and return
-      end
-
-      if @user.update(user_params)
-        redirect_to @user
-      else
-        render :edit
-      end
+    if @user.update(user_params)
+      redirect_to @user
     else
-      flash[:danger] = 'You have to login!'
-      redirect_to login_path
+      render :edit
     end
   end
 
@@ -57,5 +38,23 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:first_name, :last_name,
                                  :email, :password, :password_confirmation)
+  end
+
+  def find_user
+    @user = User.find(params[:id])
+  end
+
+  def correct_user
+    unless user_equals?(@user)
+      flash[:danger] = 'You dont have permission!'
+      redirect_to root_path and return
+    end
+  end
+
+  def require_login
+    unless logged_in?
+      flash[:danger] = 'You have to login!'
+      redirect_to login_path
+    end
   end
 end
